fcdc5cabdd4b878dc9494b0ab542a54a27ccdc79
yordanos woldeyes <107896300+yordanosW@users.noreply.github.com>
2023-10-06 13:35:17 -0700

    super

--- vehicleBaseClass.js
+++ vehicleBaseClass.js
@@ -1,3 +1,6 @@
+
+
+
 class Vehicle {
     constructor(make, model, year, color, mileage) {
         this.make = make;
@@ -88,4 +91,36 @@ class Vehicle {
 //This exports things you want to use from this "module", more info in readme
 module.exports = {
     Vehicle
+}
+
+class Car extends Vehicle{
+    constructor(name, model)
+{    super (name)
+    this.name= name;  //constructor function to create the properties of car object and assign values to them from parameters passed in it.
+    this.maximumPassenger=5;
+    this.passengers=0;
+this.numberOfWheels=4;
+this.maximumSpeed=160;
+this.fuel=10;
+this.scheduleService=false;
+}
+loadPassenger(num){
+
+}
+start(){
+if(this.fuel>0){
+    return this.started = true;
+    console.log("engine started...!!!");
+} else {
+    return this.started = false;
+    console.log("engine cannot start...");
+
+
+}
+}
+scheduleService(mileage){
+    if(this.mileage>30000){
+        return"timefor mainterance"
+        }
+}
 }
